ce341cd551f002c4382d24cbb7698db8f7dd48ef
Jon Richards <jonr.web@gmail.com>
2024-01-01 10:37:21 +0000

    better snake logic

--- src/components/SnakeBackground/constants/colours.ts
+++ src/components/SnakeBackground/constants/colours.ts
@@ -3,6 +3,7 @@ export interface SnakeColours {
   head: string;
 }
 
+// todo update colours once app palette is finalised a bit more
 export const APPLE_COLOUR = "#034f56";
 export const BACKGROUND_COLOUR = "#0F172A";
 export const SNAKE_HUES = Array.from({ length: 12 }, (_, i) => i * 10);
